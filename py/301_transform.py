62ccf773387e97558cd9b522a5b6494c40d8059c
toshiki-ishikawa <toshiki.ishikawa@dena.jp>
2019-03-07 17:27:33 +0900

    change

--- py/301_transform.py
+++ py/301_transform.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env ptargetthon3
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar 4 2019
@@ -15,73 +15,15 @@ import pandas as pd
 import xlearn as xl
 
 from tqdm import tqdm
+from sklearn.model_selection import StratifiedKFold, KFold
 
 utils.start(__file__)
 # =============================================================================
 PATH = os.path.join('..', 'input')
 
-# =============================================================================
-# DEF
-# =============================================================================
-def to_libffm_format(train, test, v):
-    currentcode = len(numerics)
-    catdict = {}
-    catcodes = {}
-    for x in numerics:
-        catdict[x] = 0
-    for x in categories:
-        catdict[x] = 1
+NFOLDS = 6
 
-    noofrows = train.shape[0]
-    with open(f"../data/alltrainffm_{v}.txt", "w") as text_file:
-        for n, r in enumerate(range(noofrows)):
-            datastring = ""
-            datarow = train.iloc[r].to_dict()
-            datastring += str(int(datarow['y']))
-
-            for i, x in enumerate(catdict.keys()):
-                if(catdict[x] == 0):
-                    datastring = datastring + " " + \
-                        str(i)+":" + str(i)+":" + str(datarow[x])
-                else:
-                    if(x not in catcodes):
-                        catcodes[x] = {}
-                        currentcode += 1
-                        catcodes[x][datarow[x]] = currentcode
-                    elif(datarow[x] not in catcodes[x]):
-                        currentcode += 1
-                        catcodes[x][datarow[x]] = currentcode
-
-                    code = catcodes[x][datarow[x]]
-                    datastring = datastring + " "+str(i)+":" + str(int(code))+":1"
-            datastring += '\n'
-            text_file.write(datastring)
-
-    noofrows = test.shape[0]
-    with open(f"../data/alltestffm_{v}.txt", "w") as text_file:
-        for n, r in enumerate(range(noofrows)): 
-            datastring = ""
-            datarow = test.iloc[r].to_dict()
-            datastring += str(int(datarow['y']))
-
-            for i, x in enumerate(catdict.keys()):
-                if(catdict[x] == 0):
-                    datastring = datastring + " " + \
-                        str(i)+":" + str(i)+":" + str(datarow[x])
-                else:
-                    if(x not in catcodes):
-                        catcodes[x] = {}
-                        currentcode += 1
-                        catcodes[x][datarow[x]] = currentcode
-                    elif(datarow[x] not in catcodes[x]):
-                        currentcode += 1
-                        catcodes[x][datarow[x]] = currentcode
-
-                    code = catcodes[x][datarow[x]]
-                    datastring = datastring + " "+str(i)+":" + str(int(code))+":1"
-            datastring += '\n'
-            text_file.write(datastring)
-
+SEED = 6
 
 # =============================================================================
 # MAIN
@@ -89,15 +31,150 @@ def to_libffm_format(train, test, v):
 
 train = pd.read_csv(os.path.join(PATH, 'train.csv'))
 test = pd.read_csv(os.path.join(PATH, 'test.csv'))
+test.insert(1, 'target', 0)
 
-categories = [c for c in train.columns if c != 'target']
-numerics = []
+for c in train.columns[2:]:
+    train[c] = np.round(train[c], 0)
+    test[c] = np.round(test[c], 0)
 
-test['y'] = 0
-for v in tqdm(train['y'].unique()):
-    if (v in y) and (v not in y_):
-        train['y'] = train['y'].apply(lambda x: np.where(x == v, 1, 0)) 
-        to_libffm_format(train, test, v)
+features = train.columns[2:]
+
+categories = []
+numerics = [c for c in train.columns if c != 'target' and c != 'ID_code']
+
+currentcode = len(numerics)
+catdict = {}
+catcodes = {}
+for x in numerics:
+    catdict[x] = 0
+for x in categories:
+    catdict[x] = 1
+
+y = train['target']
+kf = KFold(NFOLDS, shuffle=True, random_state=SEED)
+
+for fold, (train_index, valid_index) in enumerate(kf.split(train, y)):
+    print('fold: {}/{}'.format(fold+1, kf.n_splits))
+
+    train_ = train.iloc[train_index]
+    valid = train.iloc[valid_index]
+
+    # =========================================================================
+    # TRAIN
+    # =========================================================================
+
+    noofrows = train_.shape[0]
+    noofcolumns = len(features)
+
+    with open(f"../data/train_fold{fold}.txt", "w") as text_file:
+        for n, r in enumerate(range(noofrows)):
+            datastring = ""
+            datarow = train_.iloc[r].to_dict()
+            datastring += str(int(datarow['target']))
+
+            for i, x in enumerate(catdict.keys()):
+                if(catdict[x] == 0):
+                    datastring = datastring + " " + \
+                        str(i)+":" + str(i)+":" + str(datarow[x])
+                else:
+                    if(x not in catcodes):
+                        catcodes[x] = {}
+                        currentcode += 1
+                        catcodes[x][datarow[x]] = currentcode
+                    elif(datarow[x] not in catcodes[x]):
+                        currentcode += 1
+                        catcodes[x][datarow[x]] = currentcode
+
+                    code = catcodes[x][datarow[x]]
+                    datastring = datastring + " "+str(i)+":" + str(int(code))+":1"
+            datastring += '\n'
+            text_file.write(datastring)
+
+    # =========================================================================
+    # VALID
+    # =========================================================================
+
+    noofrows = valid.shape[0]
+    noofcolumns = len(features)
+
+    with open(f"../data/valid_fold{fold}.txt", "w") as text_file:
+        for n, r in enumerate(range(noofrows)):
+            datastring = ""
+            datarow = valid.iloc[r].to_dict()
+            datastring += str(int(datarow['target']))
+
+            for i, x in enumerate(catdict.keys()):
+                if(catdict[x] == 0):
+                    datastring = datastring + " " + \
+                        str(i)+":" + str(i)+":" + str(datarow[x])
+                else:
+                    if(x not in catcodes):
+                        catcodes[x] = {}
+                        currentcode += 1
+                        catcodes[x][datarow[x]] = currentcode
+                    elif(datarow[x] not in catcodes[x]):
+                        currentcode += 1
+                        catcodes[x][datarow[x]] = currentcode
+
+                    code = catcodes[x][datarow[x]]
+                    datastring = datastring + " "+str(i)+":" + str(int(code))+":1"
+            datastring += '\n'
+            text_file.write(datastring)
+
+# =============================================================================
+# TEST
+# =============================================================================
+
+noofrows = test.shape[0]
+noofcolumns = len(features)
+
+with open("../data/test.txt", "w") as text_file:
+    for n, r in enumerate(range(noofrows)): 
+        datastring = ""
+        datarow = test.iloc[r].to_dict()
+        datastring += str(int(datarow['target']))
+
+        for i, x in enumerate(catdict.keys()):
+            if(catdict[x] == 0):
+                datastring = datastring + " " + \
+                    str(i)+":" + str(i)+":" + str(datarow[x])
+            else:
+                if(x not in catcodes):
+                    catcodes[x] = {}
+                    currentcode += 1
+                    catcodes[x][datarow[x]] = currentcode
+                elif(datarow[x] not in catcodes[x]):
+                    currentcode += 1
+                    catcodes[x][datarow[x]] = currentcode
+
+                code = catcodes[x][datarow[x]]
+                datastring = datastring + " "+str(i)+":" + str(int(code))+":1"
+        datastring += '\n'
+        text_file.write(datastring)
+
+with open("../data/test.txt", "w") as text_file:
+    for n, r in enumerate(range(noofrows)): 
+        datastring = ""
+        datarow = test.iloc[r].to_dict()
+        datastring += str(int(datarow['target']))
+
+        for i, x in enumerate(catdict.keys()):
+            if(catdict[x] == 0):
+                datastring = datastring + " " + \
+                    str(i)+":" + str(i)+":" + str(datarow[x])
+            else:
+                if(x not in catcodes):
+                    catcodes[x] = {}
+                    currentcode += 1
+                    catcodes[x][datarow[x]] = currentcode
+                elif(datarow[x] not in catcodes[x]):
+                    currentcode += 1
+                    catcodes[x][datarow[x]] = currentcode
+
+                code = catcodes[x][datarow[x]]
+                datastring = datastring + " "+str(i)+":" + str(int(code))+":1"
+        datastring += '\n'
+        text_file.write(datastring)
 
 # =============================================================================
 utils.end(__file__)
---
+++ py/704_xlearn.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env ptargetthon3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 4 2019
+@author: toshiki.ishikawa
+"""
+
+import os 
+import sys
+import gc
+import utils
+
+import numpy as np
+import pandas as pd
+import xlearn as xl
+
+from tqdm import tqdm
+
+utils.start(__file__)
+# =============================================================================
+DATA_PATH = os.path.join('..', 'data')
+INPUTS_PATH = os.path.join('..', 'input')
+OUTPUTS_PATH = os.path.join('..', 'output')
+SUBMISSION_PATH = os.path.join('..', 'submission')
+
+NFOLDS = 6
+
+param = {
+    'task': 'binary',
+    'metric': 'auc', 
+    'opt': 'adagrad',
+    
+    'lr': 0.1, 
+    'lambda': 0.002, 
+    'epoch': 20,
+}
+
+# =============================================================================
+# MODEL 
+# =============================================================================
+
+for i in range(NFOLDS):
+    ffm_model = xl.create_ffm()
+    ffm_model.setTrain(os.path.join(DATA_PATH, f'train_fold{i}.txt'))
+    ffm_model.setValidate(os.path.join(DATA_PATH, f'valid_fold{i}.txt'))
+
+    ffm_model.fit(param, os.path.join(OUTPUTS_PATH, f'model_fold{i}.out'))
+
+    ffm_model.setTest(os.path.join(DATA_PATH, 'test.txt'))
+    ffm_model.setSigmoid() 
+    ffm_model.predict(os.path.join(OUTPUTS_PATH, f'model_fold{i}.out'), os.path.join(OUTPUTS_PATH, f'prediction_fold{i}.txt'))
+
+# =============================================================================
+# SUBMISSION
+# =============================================================================
+
+submission = pd.read_csv(os.path.join(INPUTS_PATH, 'sample_submission.csv'))
+for i in range(NFOLDS):
+    pred = pd.read_csv(os.path.join(OUTPUTS_PATH, f'prediction_fold{i}.txt'), header=None).rename(columns={0: 'target'}) 
+    submission['target'] = pred['target']
+
+submission['target'] = submission['target'] / NFOLDS
+submission.to_csv(os.path.join(SUBMISSION_PATH, 'xlearn.csv'), index=False)
+
+# =============================================================================
+utils.end(__file__)
