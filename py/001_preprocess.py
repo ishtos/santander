e05f8346fc349906a928f9a6b7f54cca7d51710c
ti-ginkgo <ginkgonut.9960@gmail.com>
2019-03-08 10:09:21 +0900

    delete

---
+++ py/001_preprocess.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 2018
+
+@author: toshiki.ishikawa
+"""
+
+import os
+import sys
+import gc
+import utils
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+from multiprocessing import cpu_count, Pool
+
+
+utils.start(__file__)
+#==============================================================================
+NTHREAD = cpu_count()
+
+PATH = os.path.join('..', 'input')
+
+# =============================================================================
+#
+# =============================================================================
+files = os.listdir(PATH)
+
+for f in files:
+    df = pd.read_csv(os.path.join(PATH, f))
+    df = utils.reduce_mem_usage(df)
+    df.to_csv(f'../data/{f}', index=False)
+
+# =============================================================================
+utils.end(__file__)
